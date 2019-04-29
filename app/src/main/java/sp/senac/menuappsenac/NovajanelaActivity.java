650573b08cdc1b30b5e5074227f6adf614ebdd3c
JONATAS REIS DA SILVA <jonatas.rsilva3@senacsp.edu.br>
2019-04-29 12:22:56 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/menuappsenac/NovajanelaActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.menuappsenac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovajanelaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.novajanela_layout);
+    }
+}
